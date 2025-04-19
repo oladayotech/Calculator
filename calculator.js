e35aa424a62a8bb1d2fd316a4f449f3b905a88b8
oladayotech <d1arogundade@gmail.com>
2025-04-19 23:15:23 +0100

    update calculator.js

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function calculaotion_selector() {
+    math_list = {
+        1: "Add",
+        2: "Sub",
+        3: "Multiply",
+        4: "Divide",
+        5: "Exponential",
+        6: "Square",
+        7: "Square Root"
+    }
+
+    console.log("Choose a calculation to perform")
+    // loop the above dict
+    for (let key in math_list) {
+        console.log(key, ".", math_list[key]);
+    }
+    try {
+        while (true) {
+            user_input = prompt("In")
+        }
+    } catch (error) {
+        
+    }
+}
